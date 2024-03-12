01c33ba49a3f16ed1aef9f43aec626133f55136f
Rahul-303 <rahulkumarsinghnfs@gmail.com>
2024-03-13 03:06:56 +0530

    create signup api route and done some formatting

--- api/index.js
+++ api/index.js
@@ -1,24 +1,28 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import userRoutes from './routes/user.route.js'
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
+app.use(express.json());
 dotenv.config();
+
 const port = 3000;
 const url = process.env.MONGO;
 
 mongoose
-.connect(url)
-.then(()=>{
-    console.log('connection established!');
-})
-.catch((error)=>{
+  .connect(url)
+  .then(() => {
+    console.log("connection established!");
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
 
-app.listen(port, ()=>{
-    console.log(`server listening at ${port}!`);
-})
+app.listen(port, () => {
+  console.log(`server listening at ${port}!`);
+});
 
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);

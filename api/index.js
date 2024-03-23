24cb8e9d9a911e49fc79ba19af09e671c22e57ff
Rahul-303 <rahulkumarsinghnfs@gmail.com>
2024-03-24 02:32:47 +0530

    added api route for post creation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 // import cors from "cors";
 import cookieParser from "cookie-parser";
+import postRoutes from "./routes/post.route.js";
 
 const app = express();
 app.use(express.json());
@@ -35,6 +36,7 @@ app.listen(port, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post",postRoutes)
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
---
+++ api/models/post.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://feelforhair.co.uk/wp-content/uploads/2017/12/default-post-thumbnail.png",
+    },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+export default Post;

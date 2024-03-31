bf750bebb9fcfb71d7987fab7310a52656f0904f
Rahul-303 <rahulkumarsinghnfs@gmail.com>
2024-04-01 01:01:15 +0530

    added delete post functionality to the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,88 +1,88 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { Modal, Table, Button, Spinner } from "flowbite-react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Table, Modal, Button } from "flowbite-react";
 import { Link } from "react-router-dom";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
+import axios from "axios";
 
 const DashPosts = () => {
-  const [posts, setPosts] = useState([]);
   const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
   const [showMore, setShowMore] = useState(true);
-  const [error, setError] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [postIdToDelete, setPostIdToDelete] = useState("");
   const [loading, setLoading] = useState(false);
-  const [postId, setPostId] = useState("");
 
-  const handleShowMore = async (e) => {
-    const index = posts.length;
-    e.preventDefault();
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isCreator) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
     try {
       const res = await axios.get(
-        `/api/post/getposts?userId=${currentUser._id}&startIndex=${index}`
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
       );
-      setPosts((prevPosts) => [...prevPosts, ...res.data.posts]);
+      setUserPosts((prev) => [...prev, ...res.data.posts]);
       if (res.data.posts.length < 9) {
         setShowMore(false);
       }
     } catch (error) {
-      setError(true);
+      console.log(error.message);
     }
   };
 
-  const handleDeletePost = async (e) => {
-    e.preventDefault();
+  const handleDeletePost = async () => {
     setShowModal(false);
+    setLoading(true);
     try {
       const res = await axios.delete(
-        `/api/post/deletePosts/${postId}/${currentUser._id}`
+        `/api/post/deleteposts/${postIdToDelete}/${currentUser._id}`
       );
-      setPosts((prevPost) => {
-        prevPost.filter((post) => post._id !== postId);
-      });
+      setUserPosts((prev) =>
+        prev.filter((post) => post._id !== postIdToDelete)
+      );
+      setLoading(false);
     } catch (error) {
-      console.log(error);
+      console.log(error.message);
+      setLoading(false);
     }
   };
-
-  useEffect(() => {
-    const fetch = async () => {
-      try {
-        const res = await axios.get(
-          `/api/post/getposts?userId=${currentUser._id}`
-        );
-        setPosts(res.data.posts);
-        if (res.data.posts.length < 9) {
-          setShowMore(false);
-        }
-      } catch (error) {
-        setError(true);
-      }
-    };
-    if (currentUser.isCreator) {
-      fetch();
-    }
-  }, [currentUser._id]);
 
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
-      {currentUser.isCreator && posts.length > 0 ? (
+      {currentUser.isCreator && userPosts.length > 0 ? (
         <>
           <Table hoverable className="shadow-md">
-            <Table.Head className="table-auto">
+            <Table.Head>
               <Table.HeadCell>Date updated</Table.HeadCell>
               <Table.HeadCell>Post image</Table.HeadCell>
               <Table.HeadCell>Post title</Table.HeadCell>
               <Table.HeadCell>Category</Table.HeadCell>
               <Table.HeadCell>Delete</Table.HeadCell>
-              <Table.HeadCell>Edit</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
             </Table.Head>
-            {posts.map((post) => (
+            {userPosts.map((post) => (
               <Table.Body className="divide-y">
-                <Table.Row
-                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
-                  key={post._id}
-                >
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                   <Table.Cell>
                     {new Date(post.updatedAt).toLocaleDateString()}
                   </Table.Cell>
@@ -106,20 +106,21 @@ const DashPosts = () => {
                   <Table.Cell>{post.category}</Table.Cell>
                   <Table.Cell>
                     <span
-                      className="font-medium text-red-700 hover:underline cursor-pointer"
                       onClick={() => {
-                        setPostId(post._id);
                         setShowModal(true);
+                        setPostIdToDelete(post._id);
                       }}
+                      className="font-medium text-red-500 hover:underline cursor-pointer"
                     >
                       Delete
                     </span>
                   </Table.Cell>
                   <Table.Cell>
-                    <Link to={`/update-post/${post._id}`}>
-                      <span className="font-medium text-teal-500 hover:underline cursor-pointer">
-                        Edit
-                      </span>
+                    <Link
+                      className="text-teal-500 hover:underline"
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Edit</span>
                     </Link>
                   </Table.Cell>
                 </Table.Row>
@@ -129,16 +130,12 @@ const DashPosts = () => {
           {showMore && (
             <button
               onClick={handleShowMore}
-              className="w-full self-center text-teal-500 py-2"
+              className="w-full text-teal-500 self-center text-sm py-7"
             >
-              Show More..
+              Show more
             </button>
           )}
         </>
-      ) : error ? (
-        <span className="self-center text-red-700">
-          <h2>Something went wrong!</h2>
-        </span>
       ) : (
         <p>You have no posts yet!</p>
       )}

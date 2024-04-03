027002dfba5459c76f07f4d3bd1d3288dede6cf0
Rahul-303 <rahulkumarsinghnfs@gmail.com>
2024-04-04 01:36:32 +0530

    added like functionality to the comment

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,15 +1,16 @@
-import { Button, Textarea } from "flowbite-react";
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import SingleComment from "./SingleComment";
+import { Alert, Button, TextInput, Textarea } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import SingleComment from './SingleComment';
 
-const Comment = (props) => {
+export default function Comment({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
-  const [comment, setComment] = useState("");
-  const [error, setError] = useState(null);
-  const [comments , setComments] = useState([]);
+  const [comment, setComment] = useState('');
+  const [commentError, setCommentError] = useState(null);
+  const [comments, setComments] = useState([]);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,102 +18,128 @@ const Comment = (props) => {
       return;
     }
     const config = {
-      headers: { "Content-Type": "application/json" },
-    };
-
-    const commentData = {
-      content: comment,
-      postId: props.postId,
-      userId: currentUser._id,
-    };
-
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }
     try {
-      const res = await axios.post(`/api/comment/create`, commentData, config);
-      setComment("");
-      setError(null);
-      setComments([res.data, ...comments])
+      const res = await axios.post('/api/comment/create', 
+      {
+          content: comment,
+          postId,
+          userId: currentUser._id,
+        },config);
+
+        setComment('');
+        setCommentError(null);
+        setComments([res.data, ...comments]);
     } catch (error) {
-      setError(error.response.data.message);
+      setCommentError(error.message);
     }
   };
 
-  useEffect(()=>{
-    const getComments = async() =>{
-      try{
-        const res = await axios.get(`/api/comment/getPostComment/${props.postId}`);
-        console.log(res.data);
-        setComments(res.data);
-      }catch(error){
-        console.log(error);
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await axios.get(`/api/comment/getPostComments/${postId}`);
+          setComments(res.data);
+      } catch (error) {
+        console.log(error.message);
       }
-    }
+    };
     getComments();
-  }, [props.postId])
+  }, [postId]);
+
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate('/sign-in');
+        return;
+      }
+      const res = await axios.put(`/api/comment/likeComment/${commentId}`);
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: res.data.likes,
+                  numberOfLikes: res.data.likes.length,
+                }
+              : comment
+          )
+        );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  
   return (
-    <div className="max-w-2xl mx-auto w-full p-3">
+    <div className='max-w-2xl mx-auto w-full p-3'>
       {currentUser ? (
-        <div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
-          <p>Signed in as : </p>
+        <div className='flex items-center gap-1 my-5 text-gray-500 text-sm'>
+          <p>Signed in as:</p>
           <img
-            className="h-5 w-5 object-cover rounded-full"
+            className='h-5 w-5 object-cover rounded-full'
             src={currentUser.profilePicture}
-            alt=""
+            alt=''
           />
           <Link
-            to={"/dashboard?tab=profile"}
-            className="text-xs text-cyan-600 hover:underline"
+            to={'/dashboard?tab=profile'}
+            className='text-xs text-cyan-600 hover:underline'
           >
             @{currentUser.username}
           </Link>
         </div>
       ) : (
-        <div className="text-teal-500 my-5 flex gap-1">
-          You must sign in to comment
-          <Link to={"/sign-in"} className="text-blue-500 hover:underline">
-            Sign In{" "}
+        <div className='text-sm text-teal-500 my-5 flex gap-1'>
+          You must be signed in to comment.
+          <Link className='text-blue-500 hover:underline' to={'/sign-in'}>
+            Sign In
           </Link>
         </div>
       )}
       {currentUser && (
         <form
           onSubmit={handleSubmit}
-          className="border border-teal-500 rounded-tl-xl rounded-bl-none rounded-tr-none rounded-br-xl p-3"
+          className='border border-teal-500 rounded-md p-3'
         >
           <Textarea
-            placeholder="Add a comment..."
-            rows={3}
-            maxLength="200"
+            placeholder='Add a comment...'
+            rows='3'
+            maxLength='200'
             onChange={(e) => setComment(e.target.value)}
             value={comment}
           />
-          <div className="flex justify-between items-center mt-5 ">
-            <p className="text-gray-500 text-xs">
+          <div className='flex justify-between items-center mt-5'>
+            <p className='text-gray-500 text-xs'>
               {200 - comment.length} characters remaining
             </p>
-            <Button outline gradientDuoTone="purpleToBlue" type="submit">
+            <Button outline gradientDuoTone='purpleToBlue' type='submit'>
               Submit
             </Button>
           </div>
-          {error && <span className=" text-red-700">{error}</span>}
+          {commentError && (
+            <span className='text-red-700'>
+              {commentError}
+            </span>
+          )}
         </form>
       )}
       {comments.length === 0 ? (
-        <p className="text-sm my-5"> no commenst yet!</p>
+        <p className='text-sm my-5'>No comments yet!</p>
       ) : (
         <>
-        <div className="text-sm my-5 flex items-center gap-1">
-          <p>Comments</p>
-          <div className="border border-gray-400 py-1 px-2 rounded-sm">{comments.length}</div>
-        </div>
-        {comments.map(comment => (
-          <SingleComment key={comment._id} comment={comment}/>
-        ))}
+          <div className='text-sm my-5 flex items-center gap-1'>
+            <p>Comments</p>
+            <div className='border border-gray-400 py-1 px-2 rounded-sm'>
+              <p>{comments.length}</p>
+            </div>
+          </div>
+          {comments.map((comment) => (
+            <SingleComment key={comment._id} comment={comment} onLike={handleLike} />
+          ))}
         </>
-        
-
       )}
     </div>
   );
-};
-
-export default Comment;
+}
